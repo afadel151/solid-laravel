3e22b1bf8bf4d17af1221006a9287015324710a7
Fadel Akram <af8356207@gmail.com>
2025-10-29 08:28:34 +0100

    created custom form requests for validation

---
+++ final/app/Http/Requests/Room/AddRoom.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddRoom extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "room"=> "string|required|unique:rooms,room",
+            "capacity_teaching"=> "integer|required",
+        ];
+    }
+}
