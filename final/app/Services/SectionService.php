9fd759b0f8b6feadf6beb2da0446e5492e9e3ae2
Fadel Akram <af8356207@gmail.com>
2025-10-29 22:27:46 +0100

    edited section service

--- final/app/Services/SectionService.php
+++ final/app/Services/SectionService.php
@@ -1,10 +1,33 @@
 <?php
 
 namespace App\Services;
+use App\Interfaces\SectionRepositoryInterface;
 
-use App\Interfaces\SectionServiceInterface;
-
-class SectionService implements SectionServiceInterface
+class SectionService 
 {
+    protected $sectionRepository;
+    public function __construct(SectionRepositoryInterface $sectionRepository)
+    {
+        $this->sectionRepository = $sectionRepository;
+    }
+    public function getAll(){
+        return $this->sectionRepository->all();
+    }
+
+    public function getById($id){
+        return $this->sectionRepository->find($id);
+    }
+
+    public function store(array $data){
+        $section = $this->sectionRepository->create($data);
+        return $section;
+    }
+    public function update(array $data){
+        return $this->sectionRepository->update($data);
+    }
+    public function delete($id){
+        $deleted = $this->sectionRepository->delete($id);
+        return $deleted;
+    }
 
 }
---
+++ final/app/Services/SectorService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\SectorRepositoryInterface;
+use App\Interfaces\SectorServiceInterface;
+
+class SectorService implements SectorServiceInterface
+{
+    protected $sectorrepository;
+    public function __construct(SectorRepositoryInterface $repository){
+        $this->sectorrepository = $repository;
+    }
+    public function getAll()
+    {
+        return $this->sectorrepository->all();
+    }
+    public function getById($id)
+    {
+        return $this->sectorrepository->find($id);
+    }
+    public function create(array $data)
+    {
+        $sector = $this->sectorrepository->create($data);
+        return $sector;
+    }
+    public function update(array $data)
+    {
+        $sector = $this->sectorrepository->update($data);
+        return $sector;
+    }
+    public function delete(int $id)
+    {
+        return $this->sectorrepository->delete($id);
+    }
+
+}
