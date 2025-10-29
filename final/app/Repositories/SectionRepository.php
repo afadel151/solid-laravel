d58be8406117be275b21e25bb68d347a8f498783
Fadel Akram <af8356207@gmail.com>
2025-10-29 22:27:27 +0100

    edited repositories

--- final/app/Repositories/SectionRepository.php
+++ final/app/Repositories/SectionRepository.php
@@ -3,8 +3,37 @@
 namespace App\Repositories;
 
 use App\Interfaces\SectionRepositoryInterface;
+use App\Models\Section;
 
 class SectionRepository implements SectionRepositoryInterface
 {
+    public function all()
+    {
+        return Section::all();
+    }
 
+    public function find($id)
+    {
+        return Section::find($id);
+    }
+
+    public function create(array $data)
+    {
+        return Section::create($data);
+    }
+
+    public function update(array $data)
+    {
+        $section = Section::find($data['id']);
+        if ($section) {
+            $section->update($data);
+        }
+
+        return $section;
+    }
+
+    public function delete($id)
+    {
+        return Section::find($id)->delete();
+    }
 }
--- final/app/Repositories/SectorRepository.php
+++ final/app/Repositories/SectorRepository.php
@@ -11,5 +11,25 @@ use Illuminate\Support\Collection;
 
 class SectorRepository implements SectorRepositoryInterface
 {
-    
+ 
+    public function all()
+    {
+        return Sector::all();
+    }
+    public function find($id)
+    {
+        return Sector::find($id);
+    }
+    public function create(array $data)
+    {
+        return Sector::create($data);
+    }
+    public function update(array $data)
+    {
+        return Sector::find($data['id'])->update($data);
+    }
+    public function delete(int $id): bool
+    {
+        return Sector::find($id)->delete();
+    }
 }
