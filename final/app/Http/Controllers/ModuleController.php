ba0375fc0e564a6786cbc4ff8afe6f1d451a19e0
Fadel Akram <af8356207@gmail.com>
2025-10-29 08:33:27 +0100

    edited module controler

--- final/app/Http/Controllers/ModuleController.php
+++ final/app/Http/Controllers/ModuleController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\ModuleService;
+use Inertia\Inertia;
 
 class ModuleController extends Controller
 {
-    //
+    protected $moduleSvc;
+    public function __construct(ModuleService $module_service)
+    {
+        $this->moduleSvc = $module_service;
+    }
+    public function get_all(){
+
+        return response()->json($this->moduleSvc->all());
+    }
+    public function index(){
+        return Inertia::render("modules/Index",[
+            'modules'=> $this->moduleSvc->all()
+        ]);
+    }
 }
--- final/app/Http/Controllers/YearController.php
+++ final/app/Http/Controllers/YearController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Services\YearService;
+use Illuminate\Database\Eloquent\Collection;
 
 class YearController extends Controller
 {
-    //
+    protected $yearService;
+
+    public function __construct(YearService $yearService)
+    {
+        $this->yearService = $yearService;
+    }
+
+    public function index(): Collection
+    {
+        return $this->yearService->getAllYears();
+    }
 }
