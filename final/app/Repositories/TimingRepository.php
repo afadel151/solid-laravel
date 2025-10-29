918c11ffeb86f31ec3f1ceb9e70b51e613b2d432
Fadel Akram <af8356207@gmail.com>
2025-10-29 21:23:23 +0100

    modified timing repo

--- final/app/Repositories/TimingRepository.php
+++ final/app/Repositories/TimingRepository.php
@@ -3,8 +3,29 @@
 namespace App\Repositories;
 
 use App\Interfaces\TimingRepositoryInterface;
+use App\Models\Timing;
 
 class TimingRepository implements TimingRepositoryInterface
 {
+    public function all(){
+        return Timing::all();
+    }
+    
+    public function find($id){
+        return Timing::find($id);
+    }
 
+    public function update(array $data){
+        Timing::update($data);
+    }
+
+    public function create(array $data){
+        return Timing::create($data);
+    }
+
+    public function delete($id){
+
+        return Timing::find(id: $id)->delete();
+    }
+    
 }
