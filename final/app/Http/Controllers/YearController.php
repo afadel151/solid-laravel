da457acafe6768a2be282b75b46e7fe20bd61056
Fadel Akram <af8356207@gmail.com>
2025-10-29 21:22:12 +0100

    year controller

--- final/app/Http/Controllers/YearController.php
+++ final/app/Http/Controllers/YearController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Year\AddYear;
+use App\Http\Resources\Year\YearResource;
+use App\Models\Year;
 use App\Services\YearService;
 use Illuminate\Database\Eloquent\Collection;
+use Inertia\Inertia;
 
 class YearController extends Controller
 {
@@ -17,5 +21,22 @@ class YearController extends Controller
     public function index(): Collection
     {
         return $this->yearService->getAllYears();
+    }
+
+    public function store(AddYear $request)
+    {
+        $year = $this->yearService->createYear($request->validated());
+
+        return response()->json([
+            'message' => 'Year created successfully',
+            'data' => YearResource::make($year),
+        ], 201);
+    }
+
+    public function show(Year $year)
+    {
+        return Inertia::render('years/Show', [
+            'year' => new YearResource($year),
+        ]);
     }
 }
