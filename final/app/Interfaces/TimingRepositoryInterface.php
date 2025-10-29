024146b87a290ad921f6f9c7393c57be886a10db
Fadel Akram <af8356207@gmail.com>
2025-10-29 21:23:03 +0100

    modify Timing repo interface

--- final/app/Interfaces/TimingRepositoryInterface.php
+++ final/app/Interfaces/TimingRepositoryInterface.php
@@ -4,5 +4,10 @@ namespace App\Interfaces;
 
 interface TimingRepositoryInterface
 {
-
+    public function all();
+    public function update(array $data);
+    
+    public function create(array $data);
+    public function find($id);
+    public function delete($id);
 }
