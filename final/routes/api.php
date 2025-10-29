cc54a8be76599f43a30f34016a6307ee6aa0db86
Fadel Akram <af8356207@gmail.com>
2025-10-29 21:25:12 +0100

    edited routes

--- final/routes/api.php
+++ final/routes/api.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\ModuleController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::prefix('/api')->group(function () {
-    Route::prefix('/modules')->group(function(){
-        Route::get('/',[ModuleController::class,'get_all'])->name('api.modules.get_all');
-    });
-});
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
--- final/routes/web.php
+++ final/routes/web.php
@@ -26,4 +26,3 @@ Route::prefix('modules')->group(function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
-require __DIR__.'/api.php';
