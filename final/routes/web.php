b26792eacda42a918cce93380ac88d797e5cb465
Fadel Akram <af8356207@gmail.com>
2025-10-29 08:34:20 +0100

    updated web.php

--- final/routes/web.php
+++ final/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ModuleController;
+use App\Http\Controllers\YearController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,5 +13,17 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::prefix('years')->group(function () {
+    Route::get('/',[YearController::class,'index'])->name('years.index');
+});
+
+Route::prefix('modules')->group(function () {
+    Route::get('/',[ModuleController::class,'index'])->name('modules.index');
+});
+
+
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+require __DIR__.'/api.php';
