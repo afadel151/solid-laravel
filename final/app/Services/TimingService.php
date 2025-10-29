4adb65272e03c6b4e8f4ceddebb094a725386ee0
Fadel Akram <af8356207@gmail.com>
2025-10-29 21:23:39 +0100

    modifed timing service

--- final/app/Services/TimingService.php
+++ final/app/Services/TimingService.php
@@ -2,9 +2,28 @@
 
 namespace App\Services;
 
-use App\Interfaces\TimingServiceInterface;
+use App\Interfaces\TimingRepositoryInterface;
 
-class TimingService implements TimingServiceInterface
+class TimingService 
 {
+    protected $timingRepository;
+    public function __construct(TimingRepositoryInterface $timingRepository){
+        $this->timingRepository = $timingRepository;
+    }
 
+    public function getAllTimings() {
+        return $this->timingRepository->all();
+    }
+    public function getTimingById($id) {
+        return $this->timingRepository->find($id);
+    }
+    public function updateTiming(array $data) {
+        return $this->timingRepository->update($data);
+    }
+    public function createTiming(array $data) {
+        return $this->timingRepository->create($data);
+    }
+    public function deleteTiming($id) {
+        return $this->timingRepository->delete($id);
+    }
 }
